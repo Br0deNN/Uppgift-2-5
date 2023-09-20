f640c812b2edcc8d78d11a0e0ad9d191f15624a2
7R1XY <theo.broden@gmail.com>
2023-09-20 09:50:04 +0200

    Add project files.

---
+++ Uppgift 2,5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din beräkning med två tal som inne fattar en decimal ");
+            string helaberäkning = Console.ReadLine();
+
+            int gångerindex = helaberäkning.IndexOf("*");
+
+            string tal1text = helaberäkning[..gångerindex];
+            string tal2text = helaberäkning[(gångerindex + 1)..];
+            double tal1 = double.Parse(tal1text);
+            double tal2 = double.Parse(tal2text);
+            double sum = tal1 * tal2;
+
+            Console.WriteLine("svaret är " + sum);
+            Console.ReadKey();
+        }
+    }
+}
